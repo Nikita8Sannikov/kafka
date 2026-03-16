f8334d5111a538224c7daed35ddafebe6d4ede60
nikita8sannikov <ak-4t7@mail.ru>
2026-03-16 14:50:31 +0100

    fix: fix main file

--- src/App.tsx
+++ src/App.tsx
@@ -15,13 +15,7 @@ function App() {
         <button onClick={handleClick}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
